47891d1c7b6386efd3e7334b0c55d83c182e7143
Cameron Thompson <cameron.thompson413@gmail.com>
2020-07-07 16:15:47 -0400

    Started work on tkinter,ran into problems. Will evaluate tomorrow

--- bioinformaticsGUI.py
+++ bioinformaticsGUI.py
@@ -2,3 +2,5 @@ import tkinter as tk #import tkinter as tk
 from dnaFuncs import * #import all functions from dnaFuncs
 from proteinFuncs import * #import all funcs from proteinFuncs
 from readFASTA import * #import all funcs from readFASTA
+
+window = tk.Tk()
