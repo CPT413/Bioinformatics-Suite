ae612727e966bbd15c1116451af30c4731857f83
Cameron Thompson <cameron.thompson413@gmail.com>
2020-07-07 16:07:36 -0400

    Added imports of the necessary packages

---
+++ bioinformaticsGUI.py
@@ -0,0 +1,4 @@
+import tkinter as tk #import tkinter as tk
+from dnaFuncs import * #import all functions from dnaFuncs
+from proteinFuncs import * #import all funcs from proteinFuncs
+from readFASTA import * #import all funcs from readFASTA
