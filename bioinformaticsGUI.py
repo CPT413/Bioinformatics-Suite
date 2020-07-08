dd4e1d485674f55b5a4d410d9e4ec3bf6209cf07
Cameron Thompson <cameron.thompson413@gmail.com>
2020-07-08 15:30:16 -0400

    Added output window to print analysis to

--- bioinformaticsGUI.py
+++ bioinformaticsGUI.py
@@ -10,16 +10,22 @@ def buttonClick():
     #for each button, checks if selected. if True, output is printed
     if gcVar.get() == 1:
         print(gcContent(sequence))
+        outputWindow.insert("1.0", "GC content: " + str(gcContent(sequence))+"\n")
     if revCompVar.get() == 1:
         print(reverseComp(sequence))
+        outputWindow.insert("2.0", "Reverse complement: " + reverseComp(sequence)+"\n")
     if transVar.get() == 1:
         print(transcription(sequence))
+        outputWindow.insert("3.0", "Transciption: " + transcription(sequence)+"\n")
     if translationVar.get() == 1:
         print(translation(sequence))
+        outputWindow.insert("4.0", "Translation: " + translation(sequence)+"\n")
 
 window = tk.Tk() #create tkinter winde
 window.title("Bioinformatics Suite") #changes title of window
 greeting = tk.Label(text = "Enter sequence to analyze:") #adds a test text to the window
+
+
 
 #create varaible to save the value of the checkbuttons
 sequenceEntry = tk.Entry()
@@ -37,6 +43,8 @@ translationSelect = tk.Checkbutton(text = 'Translation', variable = translationV
 #create button to analyse the seq, connected to buttonClick func
 analyzeButton = tk.Button(text = "Analyze", command = buttonClick)
 
+outputWindow = tk.Text()
+
 #pack all components into window
 greeting.pack()
 sequenceEntry.pack()
@@ -45,5 +53,6 @@ reverseCompSelect.pack()
 transcriptionSelect.pack()
 translationSelect.pack()
 analyzeButton.pack()
+outputWindow.pack()
 
 window.mainloop() #run window loop
