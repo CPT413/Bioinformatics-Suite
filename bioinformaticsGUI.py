1d32e3ecb4b4b720af1186a46d9dd7b09d71779d
Cameron Thompson <cameron.thompson413@gmail.com>
2020-07-10 12:57:31 -0400

    Made changes so that DNA suite can be launched as a secondary window to run

--- bioinformaticsGUI.py
+++ bioinformaticsGUI.py
@@ -47,54 +47,64 @@ def dnaGUI():
                     outputWindow.insert(tk.INSERT, "Translation: " + translation(sequence)+"\n")
 
 
-    window = tk.Tk() #create tkinter winde
-    window.title("Bioinformatics Suite") #changes title of window
+    dnaWindow = tk.Toplevel(master=menuWindow)
+    dnaWindow.title("DNA Suite") #changes title of window
 
      #creates and packs label for text entry box
-    entryText = tk.Label(text = "Enter sequence to analyze:")
+    entryText = tk.Label(dnaWindow,text = "Enter sequence to analyze:")
     entryText.pack()
 
     #creates and packs entry text box
-    sequenceEntry = tk.Text(height = 5, borderwidth = 2, relief = tk.GROOVE)
+    sequenceEntry = tk.Text(dnaWindow, height = 5, borderwidth = 2, relief = tk.GROOVE)
     sequenceEntry.pack()
 
     #creates and packs Radiobutton for single or FASTA input select
     seqTypeVar = tk.IntVar() #variable to store Radiobutton value
     #value of 1 == single seq, value == 2 FASTA
-    singleSeq = tk.Radiobutton(text = "Single Sequence", variable = seqTypeVar, value = 1)
-    multiSeq = tk.Radiobutton(text = "FASTA", variable = seqTypeVar, value = 2)
+    singleSeq = tk.Radiobutton(dnaWindow, text = "Single Sequence", variable = seqTypeVar, value = 1)
+    multiSeq = tk.Radiobutton(dnaWindow, text = "FASTA", variable = seqTypeVar, value = 2)
     singleSeq.pack()
     multiSeq.pack()
 
     #creates and packs lable of analysis options
-    analysisText = tk.Label(text = 'Analysis Options:')
+    analysisText = tk.Label(dnaWindow, text = 'Analysis Options:')
     analysisText.pack()
 
     #create and pack checkbuttons for analysis options
     #crates variables to store analysis variable options to
     gcVar = tk.IntVar()
-    gcContentSelect = tk.Checkbutton(text = 'GC Content', variable = gcVar)
+    gcContentSelect = tk.Checkbutton(dnaWindow, text = 'GC Content', variable = gcVar)
     gcContentSelect.pack()
     revCompVar = tk.IntVar()
-    reverseCompSelect = tk.Checkbutton(text = 'Reverse Complement', variable = revCompVar)
+    reverseCompSelect = tk.Checkbutton(dnaWindow, text = 'Reverse Complement', variable = revCompVar)
     reverseCompSelect.pack()
     transVar = tk.IntVar()
-    transcriptionSelect = tk.Checkbutton(text = 'Transciption', variable = transVar)
+    transcriptionSelect = tk.Checkbutton(dnaWindow, text = 'Transciption', variable = transVar)
     transcriptionSelect.pack()
     translationVar = tk.IntVar()
-    translationSelect = tk.Checkbutton(text = 'Translation', variable = translationVar)
+    translationSelect = tk.Checkbutton(dnaWindow, text = 'Translation', variable = translationVar)
     translationSelect.pack()
 
     #create and pack button to analyse the seq, connected to buttonClick func
-    analyzeButton = tk.Button(text = "Analyze", command = buttonClick)
+    analyzeButton = tk.Button(dnaWindow, text = "Analyze", command = buttonClick)
     analyzeButton.pack()
 
     #create and pack output box label
-    outputText = tk.Label(text = "Analysis Output:") #add text to denote output
+    outputText = tk.Label(dnaWindow, text = "Analysis Output:") #add text to denote output
     outputText.pack()
 
     #create and pack output box
-    outputWindow = tk.Text(height = 10, borderwidth = 2, relief = tk.GROOVE)
+    outputWindow = tk.Text(dnaWindow, height = 10, borderwidth = 2, relief = tk.GROOVE)
     outputWindow.pack()
 
     window.mainloop() #run window loop
+
+#create and label menu window
+menuWindow = tk.Tk()
+menuWindow.title("Bioinformatics Suite Menu")
+
+dnaButton = tk.Button(text = "DNA Suite", command = dnaGUI)
+dnaButton.pack()
+
+
+menuWindow.mainloop()
