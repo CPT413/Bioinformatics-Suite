f8268de42a3a8a9cd1f6d55b66a9c5300ba0fab0
Cameron Thompson <cameron.thompson413@gmail.com>
2020-07-10 10:46:19 -0400

    Started to adjust the func to allow text as input and not file

--- readFASTA.py
+++ readFASTA.py
@@ -1,22 +1,21 @@
-def readFASTA(filePath):
+def readFASTA(text):
     """
-    Function that allows for the reading of a FASTA formatted file.
-    The function takes the filepath of the file as input and returns a
+    Function that allows for the reading of a FASTA formatted sequence entry.
+    The function takes the text as input and returns a
     dictionary containing the header and sequence for each entry.
     """
     sequenceDictionary = {} #creates a new dictionary to add header and seq data
-    with open(filePath) as file: #opens the file at the given file path
-        for line in file: #loops over every line in the file
-            if line[0] == ">": #the lines containing the header info star with
-                               #">"
-                header = ''.join(line[1:].strip()) #takes everything after the
-                                                   #">" and makes into a string
-                                                   #the strip removes \n
-                sequence = 'holder' #creates a holder variable for the sequence
-            else: #if a line does not start with ">" then it is a seq
-                sequence = ''.join(line[:].strip())
+    for line in text: #loops over every line in the file
+        if line[0] == ">": #the lines containing the header info star with
+                           #">"
+            header = ''.join(line[1:].strip()) #takes everything after the
+                                               #">" and makes into a string
+                                               #the strip removes \n
+            sequence = 'holder' #creates a holder variable for the sequence
+        else: #if a line does not start with ">" then it is a seq
+            sequence = ''.join(line[:].strip())
 
-            sequenceDictionary[header] = sequence #first pass, the holder is
-                                            #added, second time the seq is added
+        sequenceDictionary[header] = sequence #first pass, the holder is
+                                        #added, second time the seq is added
 
     return(sequenceDictionary)
