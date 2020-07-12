0f9924066f59a8e951d4f60e82a6aed07d1ae0cf
Cameron Thompson <cameron.thompson413@gmail.com>
2020-07-12 16:26:12 -0400

    Defined and described the functions invovled in the DNA analysis

--- Website/dnaFuncs.js
+++ Website/dnaFuncs.js
@@ -1,5 +1,41 @@
 const dnaSeqCheck = sequence => {
-  return (sequence)
-}
+  /*
+  This function will be used to proofread the entered sequence before
+  processing. The function qill return a sequence in lower-case format
+  */
 
-console.log('hello')
+}
+ const gcContent = sequence => {
+  /*
+  This function will calculate the gc content for a given sequence. Returns
+  the percent gc.
+  */
+
+ }
+
+ const reverseComp = sequence => {
+  /*
+  This function will allow for the reverse complement of an input sequence to
+  be determined. Returns a string that is reverse complement.
+  */
+
+ }
+
+ const transcription = sequence => {
+  /*
+  This funcion allows for the transcription of dna to rna for the input
+  sequence. Returns a string that is the rna transcript.
+  */
+
+ }
+
+ const translation = sequence => {
+  /*
+  This function will allow for the DNA sequence to be translated into the
+  corresponding protein sequence. It finds the first start codon (ATG) in the
+  sequence and all of the stop codons (TAA, TAG, TGA). The protein will be
+  translated using the start and first stop codon. The translated protein
+  is in the 1-letter code.
+  */
+
+ }
