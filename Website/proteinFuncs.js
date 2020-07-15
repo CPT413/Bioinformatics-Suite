57e7d55cf79bf957b48083e5619130f32618aad7
Cameron Thompson <cameron.thompson413@gmail.com>
2020-07-15 09:36:13 -0400

    Started work on proteinFuncs

---
+++ Website/proteinFuncs.js
@@ -0,0 +1,50 @@
+const proteinSeqCheck = sequence => {
+  /*
+  This function will check to make sure that only applicable characters are
+  in the sequence. Will also convert the string to upper-case, which is
+  standard for working with amino acids.
+  */
+
+}
+
+const molecularWeight = sequence => {
+  /*
+  This function will take a sequence of amino acids (single letter
+  abbrevation and upper case) and will return the molecular weight of the
+  protein (g/mol).
+  */
+
+}
+
+const oneToThree = sequence => {
+  /*
+  Converts a sequence of amino acids in single-letter abbreviation to the
+  corresponding three-letter abbreviation
+  */
+
+}
+
+const threeToOne = sequence => {
+  /*
+  Converts a sequence of amino acids in three-letter abbreviation to the
+  corresponding single-letter abbreviation
+  */
+
+}
+
+const isoelectricPoint = sequence => {
+  /*
+  This function will determine the isoelectric point of the given amino acids
+  sequence. The calculation will be made assuming body temperature (37C) and
+  standard conditions.
+  */
+
+}
+
+const netCharge = (sequence, pH) => sequence{
+  /*
+  This function will be used to determine the net charge of a protein at a
+  specified pH value.
+  */
+
+}
