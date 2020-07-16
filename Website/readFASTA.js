532a5e9c2de6b2df3f290c7bdef8cf55d01d496d
Cameron Thompson <cameron.thompson413@gmail.com>
2020-07-16 18:49:42 -0400

    Finished and annotated readFASTA func.

--- Website/readFASTA.js
+++ Website/readFASTA.js
@@ -4,12 +4,16 @@ const readFASTA = text => {
   The function takes the text as input and returns a
   dictionary containing the header and sequence for each entry.
   */
+
+  //creats empty object to add the header and sequence data to.
   let sequences = {};
 
+  //loops over whole line of text, focuses on every other entry these hold
+  //the header information
   for (let line = 0; line < text.split('\n').length; line += 2) {
+    //crated object entry with the header as the key and the sequence as
+    //the value
     sequences[text.split('\n')[line].slice(1)] = text.split('\n')[line + 1];
   }
-  console.log(sequences)
+  return sequences; //return the object
 }
-
-readFASTA('>seq1\natcgcgatatagcgcta\n>seq2\natcgatctattacgt');
